1cc58810349b8b2ca47ae2772eb806731fff26bc
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-31 09:22:45 -0700

    05312020  0922

---
+++ models/books.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Books = sequelize.define('Books', {
+    title: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Please provide a title'
+        }
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'Please provide an author'
+        }
+      }
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {});
+
+  Books.associate = function(models) {
+    // associations can be defined here
+  };
+  return Books;
+};
---
+++ routes/books.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const Books = require('../models').Books;
+
+/* GET home page. */
+
+
+module.exports = router;
