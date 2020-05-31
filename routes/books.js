dc29b8bab1fe5342779e43fcbbc0adf8b5f15c37
Stheven Cabral <stheven.cabral@gmail.com>
2020-05-31 11:58:07 -0700

    05312020  1158

--- routes/books.js
+++ routes/books.js
@@ -2,7 +2,50 @@ const express = require('express');
 const router = express.Router();
 const Books = require('../models').Books;
 
-/* GET home page. */
+/***Handler function used to wrap each route */
+function asyncHandler(callback) {
+  return async (req, res, next) => {
+    try {
+      await callback(reg, res, next)
+    } catch(error) {
+      // Sets the HTTP status for the response to 500.
+      res.status(500).send(error);
+    }
+  }
+}
 
+// get /books redirected from / in index.js - Shows the full list of books.
+router.get("/", asyncHandler((req, res) => {
+  res.render("index", {});
+}));
+
+// get /books/new - Shows the create new book form.
+router.get('/new', asyncHandler((req, res) => {
+  res.render("new-book", {});
+}));
+
+// post /books/new - Posts a new book to the database.
+// /books is the url the new book is posted to.
+router.post("/", asyncHandler((req, res) => {
+  // Add article id
+  res.redirect("/books/");
+}));
+
+// get /books/:id - Shows book detail form.
+router.get("/:id", asyncHandler((req, res) => {
+  res.render("show", {});
+}));
+
+// post /books/:id - Updates book info in the database.
+router.get("/:id/edit", asyncHanlder((req, res) => {
+  res.render("update-book", {});
+}));
+
+// post /books/:id/delete - Deletes a book. Careful, this can’t be undone. It can be helpful to create a new “test” book to test deleting.
+// router.post('/:id/delete', asyncHandler(async (req ,res) => {
+//   const article = await Books.findByPk(req.params.id);
+//   await article.destroy();
+//   res.redirect("/books");
+// }));
 
 module.exports = router;
